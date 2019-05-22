2259e8e57fd9d83b78d833e58b2890a263fca908
Andhitia Rama <andhitia.r@gmail.com>
2019-05-22 13:45:21 +0700

    [8.0.2.0.0] contract_business_requirement

--- contract_business_requirement/models/account_analytic_account.py
+++ contract_business_requirement/models/account_analytic_account.py
@@ -17,6 +17,8 @@ class AccountAnalyticAccount(models.Model):
         for acc in self:
             br_ids = []
             deliverable_ids = []
+            total_revenue = resource_task_total = \
+                resource_procurement_total = gross_profit = 0.0
             criteria = [
                 ("analytic_account_id", "=", acc.id)
             ]
@@ -25,8 +27,16 @@ class AccountAnalyticAccount(models.Model):
                 br_ids += project.br_ids.ids
                 for br in project.br_ids:
                     deliverable_ids += br.deliverable_lines.ids
+                    total_revenue += br.total_revenue
+                    resource_task_total += br.resource_task_total
+                    resource_procurement_total += br.resource_procurement_total
+                    gross_profit += br.gross_profit
             acc.business_requirement_ids = br_ids
             acc.business_requirement_deliverable_ids = deliverable_ids
+            acc.total_revenue = total_revenue
+            acc.resource_task_total = resource_task_total
+            acc.resource_procurement_total = resource_procurement_total
+            acc.gross_profit = gross_profit
 
     business_requirement_ids = fields.Many2many(
         string="Business Requirements",
@@ -37,4 +47,24 @@ class AccountAnalyticAccount(models.Model):
         string="Deliverables",
         comodel_name="business.requirement.deliverable",
         compute="_compute_business_requirement",
+    )
+    total_revenue = fields.Float(
+        string="Total Revenue",
+        compute="_compute_business_requirement",
+        store=False,
+    )
+    resource_task_total = fields.Float(
+        string="Total Tasks",
+        compute="_compute_business_requirement",
+        store=False,
+    )
+    resource_procurement_total = fields.Float(
+        string="Total Procurement",
+        compute="_compute_business_requirement",
+        store=False,
+    )
+    gross_profit = fields.Float(
+        string="Estimated Gross Profit",
+        compute="_compute_business_requirement",
+        store=False,
     )
