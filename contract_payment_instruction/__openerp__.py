dde4979833cedd0443b4062bbf4f1f7d222383fb
Andhitia Rama <andhitia.r@gmail.com>
2019-05-22 08:36:15 +0700

    [8.0.1.0.0] contract_payment_instruction

---
+++ contract_payment_instruction/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Contract's Payment Instruction",
+    "version": "8.0.1.0.0",
+    "category": "Business Requirements Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_analytic_analysis",
+    ],
+    "data": [
+        "views/account_analytic_account_views.xml",
+    ],
+}
