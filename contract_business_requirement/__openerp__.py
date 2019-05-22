50ba82a61343c4116a2ba0293bba52f5b9ade201
Andhitia Rama <andhitia.r@gmail.com>
2019-05-22 13:45:21 +0700

    [8.0.2.0.0] contract_business_requirement

--- contract_business_requirement/__openerp__.py
+++ contract_business_requirement/__openerp__.py
@@ -3,14 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Contract's Business Requirement Information",
-    "version": "8.0.1.0.0",
+    "version": "8.0.2.0.0",
     "category": "Business Requirements Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "business_requirement",
+        "business_requirement_deliverable_cost",
         "account_analytic_analysis",
     ],
     "data": [
