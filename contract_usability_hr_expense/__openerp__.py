cfe2a2bf2fe674e74d5654fa9d6cb6d9ae1fe65e
Andhitia Rama <andhitia.r@gmail.com>
2019-05-31 19:18:24 +0700

    [8.0.1.0.0] contract_usability_hr_expense

---
+++ contract_usability_hr_expense/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Contract Usability - Expense",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Hidden/Dependency",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "contract_usability",
+        "hr_expense",
+    ],
+    "data": [
+        "views/account_analytic_account_views.xml",
+    ],
+}
