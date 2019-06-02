ae2402b6fba67a7a8bf9fb7d9a4ecd13d2582e21
Andhitia Rama <andhitia.r@gmail.com>
2019-06-02 22:11:43 +0700

    [8.0.1.0.1] contract_invoice_from_business_requirement
    * Add partner_id field on contract term
    * Add view and menu for contract term

--- contract_invoice_from_business_requirement/__openerp__.py
+++ contract_invoice_from_business_requirement/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Invoice From Business Requirement",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Business Requirements Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -16,5 +16,6 @@
         "security/ir.model.access.csv",
         "wizards/create_invoice_from_term.xml",
         "views/account_analytic_account_views.xml",
+        "views/account_analytic_account_invoice_term_views.xml",
     ],
 }
